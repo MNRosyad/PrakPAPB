34e560e7e71e634fae18a2fa60054323ecd5f54e
MNRosyad <muhammad.naufal2803@mail.ugm.ac.id>
2023-02-22 09:26:15 +0700

    pertamax

---
+++ app/src/main/java/com/example/ppapb/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ppapb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fragmentexample1updated.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
